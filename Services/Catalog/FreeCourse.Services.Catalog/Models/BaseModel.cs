564705dee3361c3a2b6642d28f78c89de8ef80ec
Emre Kisaboyun <emre.kisaboyun@bs.nttdata.com>
2022-12-12 18:08:06 +0300

    FreeCourse.Service.Catalog microservice added.

---
+++ Services/Catalog/FreeCourse.Services.Catalog/Models/BaseModel.cs
@@ -0,0 +1,11 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace FreeCourse.Services.Catalog.Models
+{
+    public class BaseModel
+    {
+        [BsonId]
+        [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+        public string? Id { get; set; }
+    }
+}
---
+++ Services/Catalog/FreeCourse.Services.Catalog/Models/Category.cs
@@ -0,0 +1,9 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace FreeCourse.Services.Catalog.Models
+{
+    public class Category : BaseModel
+    {
+        public string? Name { get; set; }
+    }
+}
