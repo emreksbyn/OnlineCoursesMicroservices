5139449fb7c0594a415e82af0f4edac818655eca
Emre Kisaboyun <emre.kisaboyun@bs.nttdata.com>
2022-12-23 23:10:35 +0300

    FakePayment service added.

---
+++ Services/FakePayment/FreeCourse.Services.FakePayment/Controllers/FakePaymentsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shared.FreeCourse.Shared.ControllerBases;
+using Shared.FreeCourse.Shared.Dtos;
+
+namespace FreeCourse.Services.FakePayment.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FakePaymentsController : CustomBaseController
+    {
+        [HttpPost]
+        public IActionResult ReceivePayment()
+        {
+            return CreateActionResultInstance<NoContent>(Response<NoContent>.Success(200));
+        }
+    }
+}
