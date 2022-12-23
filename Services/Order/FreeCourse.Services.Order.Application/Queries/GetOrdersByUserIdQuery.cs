ca2b116aa01151cc40a0186e1c36e6fbbd7a0e0a
Emre Kisaboyun <emre.kisaboyun@bs.nttdata.com>
2022-12-23 15:26:15 +0300

    MediatR added and GetOrdersByUserIdQuery added.

---
+++ Services/Order/FreeCourse.Services.Order.Application/Queries/GetOrdersByUserIdQuery.cs
@@ -0,0 +1,12 @@
+﻿using FreeCourse.Services.Order.Application.Dtos;
+using MediatR;
+using Shared.FreeCourse.Shared.Dtos;
+
+namespace FreeCourse.Services.Order.Application.Queries
+{
+    //                                    Response olarak -> Response<List<OrderDto>>
+    public class GetOrdersByUserIdQuery : IRequest<Response<List<OrderDto>>>
+    {
+        public string UserId { get; set; }
+    }
+}
