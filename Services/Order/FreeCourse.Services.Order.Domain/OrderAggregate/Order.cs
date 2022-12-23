b8766e5122b86dc63d82439a65d52105b6d2d938
Emre Kisaboyun <emre.kisaboyun@bs.nttdata.com>
2022-12-23 14:36:12 +0300

    Domain and Infrastructure layers have set.

---
+++ Services/Order/FreeCourse.Services.Order.Domain/OrderAggregate/Order.cs
@@ -0,0 +1,41 @@
+﻿using FreeCourse.Services.Order.Domain.Core;
+
+namespace FreeCourse.Services.Order.Domain.OrderAggregate
+{
+    public class Order : Entity, IAggregateRoot
+    {
+        public DateTime CreatedDate { get; set; }
+
+        // Owned Entity Types
+        // Ayarladigimiz takdirde Ef Core Address' e ait property' leri Order tablosuna kolon olarak ekler. Address icin ayri bir tablo olusturmaz.
+        public Address? Address { get; set; }
+        
+        public string? BuyerId { get; set; }
+
+        // Backing Field -> Encapculation yaptik. EfCore bu field' i doldurur. Asagidaki property ile de okuyabilecek hale getiriyoruz.
+        private readonly List<OrderItem> _orderItems;
+        public IReadOnlyCollection<OrderItem> OrderItems => _orderItems;
+
+        
+        public Order(string buyerId, Address address)
+        {
+            _orderItems = new List<OrderItem>();
+            CreatedDate = DateTime.Now;
+            Address = address;
+            BuyerId = buyerId;
+        }
+
+        
+        public void AddOrderItem(string productId, string productName, decimal price, string pictureUrl)
+        {
+            bool existProduct = _orderItems.Any(x => x.ProductId == productId);
+            if (!existProduct)
+            {
+                OrderItem newOrderItem = new OrderItem(productId, productName, pictureUrl, price);
+                _orderItems.Add(newOrderItem);
+            }
+        }
+
+        public decimal GetTotalPrice => _orderItems.Sum(orderItem => orderItem.Price);
+    }
+}
---
+++ Services/Order/FreeCourse.Services.Order.Domain/OrderAggregate/OrderItem.cs
@@ -0,0 +1,30 @@
+﻿using FreeCourse.Services.Order.Domain.Core;
+
+namespace FreeCourse.Services.Order.Domain.OrderAggregate
+{
+    public class OrderItem : Entity
+    {
+        public string? ProductId { get; private set; }
+        public string? ProductName { get; private set; }
+        public string? PictureUrl { get; private set; }
+        public decimal Price { get; private set; }
+
+        // Shallow Property
+        // EF Core database de OrderId diye bir kolon bulunduracaktir fakat burada property olarak tanimlamamiz gerekmez.
+        
+        public OrderItem(string? productId, string? productName, string? pictureUrl, decimal price)
+        {
+            ProductId = productId;
+            ProductName = productName;
+            PictureUrl = pictureUrl;
+            Price = price;
+        }
+
+        public void UpdateOrderItem(string productName, string pictureUrl, decimal price)
+        {
+            ProductName = productName;
+            PictureUrl = pictureUrl;
+            Price = price;
+        }
+    }
+}
