7f33a653368c88b39bad57257741c582f3d6f3cd
Emre Kisaboyun <emre.kisaboyun@bs.nttdata.com>
2022-12-23 14:57:36 +0300

    AutoMapper and Dtos added.

---
+++ Services/Order/FreeCourse.Services.Order.Application/Mapping/ObjectMapper.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+
+namespace FreeCourse.Services.Order.Application.Mapping
+{
+    //  Normalde static ise  uygulama ayaga kalkarken initialize edilir.
+    public static class ObjectMapper
+    {
+        // Sadece istenildigi anda initialize edilsin istiyorsak Lazy kullanilir.
+        private static readonly Lazy<IMapper> lazy = new Lazy<IMapper>(() =>
+        {
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<CustomMapping>();
+            });
+
+            return config.CreateMapper();
+        });
+
+        public static IMapper Mapper => lazy.Value;
+    }
+}
