480e1789aabc00cb93ff5e61f41ebbe4ed454ef4
Emre Kisaboyun <emre.kisaboyun@bs.nttdata.com>
2022-12-24 16:39:01 +0300

    ServiceApiSettings added to Web. We use it by IOptions<ServiceApiSettings>

--- Clients/FreeCourse.Web/Program.cs
+++ Clients/FreeCourse.Web/Program.cs
@@ -1,11 +1,13 @@
+using FreeCourse.Web.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.Configure<ServiceApiSettings>(builder.Configuration.GetSection("ServiceApiSettings"));
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
