0e9eab9906d2d93e02a554df2a4388a95eb64ceb
Emre Kisaboyun <emre.kisaboyun@bs.nttdata.com>
2022-12-24 16:31:38 +0300

    IdentityModel package download and IIdentityService interface added.

---
+++ Clients/FreeCourse.Web/Models/SignInInput.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FreeCourse.Web.Models
+{
+    public class SigninInput
+    {
+        [Display(Name ="Your email")]
+        public string? Email { get; set; }
+        [Display(Name = "Your password")]
+        public string? Password { get; set; }
+        [Display(Name = "Remember me")]
+        public bool IsRemember { get; set; }
+    }
+}
---
+++ Clients/FreeCourse.Web/Services/Interfaces/IIdentityService.cs
@@ -0,0 +1,17 @@
+﻿using FreeCourse.Web.Models;
+using IdentityModel.Client;
+using Shared.FreeCourse.Shared.Dtos;
+
+namespace FreeCourse.Web.Services.Interfaces
+{
+    public interface IIdentityService
+    {
+        Task<Response<bool>> SignIn(SigninInput signinInput);
+
+        // Refresh Token' i Cookie' den okuyacagimiz icin parametre almasina gerek yok.
+        Task<TokenResponse> GetAccessTokenByRefreshToken();
+
+        // Kullanici Log out olunca tokenlari silecek olan metot.
+        Task RevokeRefreshToken();
+    }
+}
